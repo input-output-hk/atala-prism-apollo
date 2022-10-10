ff5ad11116792dbd3fb59dfb99d35f8275eefa7e
Ahmed Moussa <ahmed.moussa@iohk.io>
2022-10-10 21:15:17 +0200

    feat: ValidateClaimsResult

---
+++ JWT/src/commonMain/kotlin/io.iohk.prism.jwt/ValidateClaimsResult.kt
@@ -0,0 +1,41 @@
+package io.iohk.prism.jwt
+
+/**
+ * ValidateClaimsResult list the possible results of a call to JWT.validateClaims method.
+ */
+enum class ValidateClaimsResult(val description: String) {
+    /**
+     * Successful validation.
+     */
+    SUCCESS("Success"),
+
+    /**
+     * Invalid Expiration claim.
+     */
+    INVALID_EXPIRATION("Invalid Expiration claim"),
+
+    /**
+     * Expired token: expiration time claim is in the past.
+     */
+    EXPIRED("Expired token"),
+
+    /**
+     * Invalid Not Before claim.
+     */
+    INVALID_NOT_BEFORE("Invalid Not Before claim"),
+
+    /**
+     * Not Before claim is in the future.
+     */
+    NOT_BEFORE("Token is not valid yet, Not Before claim is greater than the current time"),
+
+    /**
+     * Invalid Issued At claim.
+     */
+    INVALID_ISSUED_AT("Invalid Issued At claim"),
+
+    /**
+     * Issued At claim is in the future.
+     */
+    ISSUED_AT("Issued At claim is greater than the current time");
+}
