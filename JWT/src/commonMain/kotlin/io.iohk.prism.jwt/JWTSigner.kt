6f43debcb4946b8964c32f1b7faabd4b92085331
Ahmed Moussa <ahmed.moussa@iohk.io>
2022-10-10 21:15:07 +0200

    feat: JWTSigner

---
+++ JWT/src/commonMain/kotlin/io.iohk.prism.jwt/JWTSigner.kt
@@ -0,0 +1,50 @@
+package io.iohk.prism.jwt
+
+import kotlin.jvm.JvmStatic
+
+/**
+ * JWTSigner will be used to sign the JWT `Header` and `Claims` and generate a signed JWT.
+ */
+class JWTSigner(
+    /**
+     * The name of the algorithm that will be set in the "alg" header
+     */
+    val name: String,
+    private val signerAlgorithm: SignerAlgorithm
+) {
+    fun sign(header: String, claims: String) : String {
+        return signerAlgorithm.sign(header, claims)
+    }
+
+    companion object {
+        /**
+         * Initialize a JWTSigner using the HMAC 256 bits algorithm and the provided privateKey.
+         *
+         * @param key The HMAC symmetric password data.
+         */
+        @JvmStatic
+        fun hs256(key: ByteArray) : JWTSigner {
+            return JWTSigner("HS256", HMACAlgorithm(key, HMACAlgorithm.HMACAlgo.SHA256))
+        }
+
+        /**
+         * Initialize a JWTSigner using the HMAC 384 bits algorithm and the provided privateKey.
+         *
+         * @param key The HMAC symmetric password data.
+         */
+        @JvmStatic
+        fun hs384(key: ByteArray) : JWTSigner {
+            return JWTSigner("HS384", HMACAlgorithm(key, HMACAlgorithm.HMACAlgo.SHA384))
+        }
+
+        /**
+         * Initialize a JWTSigner using the HMAC 512 bits algorithm and the provided privateKey.
+         *
+         * @param key The HMAC symmetric password data.
+         */
+        @JvmStatic
+        fun hs512(key: ByteArray) : JWTSigner {
+            return JWTSigner("HS512", HMACAlgorithm(key, HMACAlgorithm.HMACAlgo.SHA512))
+        }
+    }
+}
