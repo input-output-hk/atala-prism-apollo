3eeb057dc81e733863dc788103b5d8dc53d1b2c2
Ahmed Moussa <ahmed.moussa@iohk.io>
2022-10-10 21:15:27 +0200

    feat: JWTError

---
+++ JWT/src/commonMain/kotlin/io.iohk.prism.jwt/Exceptions.kt
@@ -0,0 +1,6 @@
+package io.iohk.prism.jwt
+
+sealed class JWTError(message: String?) : Exception(message) {
+    class InvalidJWTString(message: String? = "Input was not a valid JWT String") : JWTError(message)
+    class FailedVerification(message: String? = "JWT verifier failed to verify the JWT String signature") : JWTError(message)
+}
